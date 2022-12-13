88dc2df656a063fe6bf7dd99fabea06025b68093
sudodou <wsdosh@gmail.com>
2022-12-13 16:49:05 +0800

    update

--- luci-app-webnavigation/luasrc/controller/webnavigation.lua
+++ luci-app-webnavigation/luasrc/controller/webnavigation.lua
@@ -8,6 +8,6 @@ function index()
 		return
 	end
 --	entry({"admin", "status", "webnavigation"}, cbi("webnavigation"), _("Webnavigation"), 55).dependent = true
-	entry({"admin", "status", "webnavigation", "config-subscribe"},cbi("webnavigation/config-subscribe"),_("Config Update超级无敌"), 60).leaf = true
-	entry({"admin", "status", "webnavigation", "config-subscribe-edit"},cbi("webnavigation/config-subscribe-edit"), nil).leaf = true
+	entry({"admin", "status", "webnavigation", "webnavigation"},cbi("webnavigation/webnavigation-show"),_("Config Update超级无敌"), 60).leaf = true
+	entry({"admin", "status", "webnavigation", "webnavigation-edit"},cbi("webnavigation/webnavigation-edit"), nil).leaf = true
 end
